194b04f716530e8fe6f9932bf5b0a48950e957be
braxtonhall <braxtonjhall@gmail.com>
2025-04-18 23:00:09 +0200

    Download reviews

--- scripts/fetch-reviews.ts
+++ scripts/fetch-reviews.ts
@@ -1,9 +1,34 @@
 import { parse } from "node-html-parser";
+import AsyncPool from "./util/async-pool";
+import * as fs from "node:fs/promises";
+import path from "path";
 
 const args = process.argv.slice(2);
 const username = args[0] ?? "ziyiyan";
 const baseUrl = "https://letterboxd.com";
 const url = `${baseUrl}/${username}/films/reviews/`;
+
+type LetterboxdInfo = {
+	url: string;
+	heart: boolean;
+	rating: number | null;
+	rewatch: boolean;
+	year: number;
+	month: number;
+	day: number;
+	movie: {
+		slug: string;
+		title: string;
+		year: number;
+		url: string;
+	};
+};
+
+type RemainingReviewInfo = { tags: string[]; text: string; language: string; spoiler: boolean };
+
+type Images = { poster: string | null; backdrop: string | null };
+
+type CompletedInfo = LetterboxdInfo & RemainingReviewInfo & { movie: Images };
 
 const getDocument = async (url: string) => {
 	const result = await fetch(url);
@@ -11,45 +36,96 @@ const getDocument = async (url: string) => {
 	return parse(text);
 };
 
-/**
- * "li.film-detail", [
- * 			{
- * 				slug: "> div.film-poster@data-film-slug",
- * 				reviewHtml: "div.js-review div.body-text@html",
- * 			},
- * 		]
- * @param url
- */
-
-const scrapePage = async (url: string) => {
-	const document = await getDocument(url);
-	const reviews = document.querySelectorAll("li.film-detail").map((element) => {
-		const paragraphs = element.querySelectorAll(".js-review .body-text p");
-		const review = paragraphs.map((paragraph) => paragraph.innerText).join("\n");
-		const slug = element.querySelector("div.film-poster")?.getAttribute("data-film-slug");
+const scrapeReviewListPage = async (
+	url: string,
+	pool: AsyncPool,
+): Promise<{ reviews: LetterboxdInfo[]; next?: string }> => {
+	const document = await pool.run(getDocument, url);
+	const reviews = document.querySelectorAll("li.film-detail").map((element): LetterboxdInfo => {
+		const poster = element.querySelector("div.film-poster");
+		const slug = poster?.getAttribute("data-film-slug");
+		const movieUrl = poster?.getAttribute("data-target-link");
 		const anchor = element.querySelector(".headline-2 a");
 		const link = anchor?.getAttribute("href");
+		const ratingElement = element.querySelector("span.rating");
+		const ratingClass =
+			ratingElement && Array.from(ratingElement.classList.values()).find((className) => className.startsWith("rated-"));
+		const date = new Date(element.querySelector(".content-metadata .date ._nobr")?.textContent.trim());
 		return {
-			text: review,
 			url: link && `${baseUrl}${link}`,
 			heart: !!element.querySelector(".icon-liked"),
-			rating: "TODO",
-			rewatch: "TODO",
+			rating: ratingClass ? Number(ratingClass.replace("rated-", "")) / 2 : null,
+			rewatch: !!element.querySelector(".content-metadata .date")?.text?.includes("Rewatched"),
+			year: date.getFullYear(),
+			month: date.getMonth() + 1,
+			day: date.getDate(),
 			movie: {
 				slug,
-				title: anchor?.innerText,
+				title: anchor?.textContent,
 				year: Number(element.querySelector(".headline-2 .metadata a").innerText),
+				url: movieUrl && `${baseUrl}${movieUrl}`,
 			},
 		};
 	});
 	const next = document.querySelector("a.next")?.getAttribute("href");
+	return { reviews, next: next && `${baseUrl}${next}` };
+};
 
+const getReviewInfo = async (url: string, pool: AsyncPool): Promise<RemainingReviewInfo> => {
+	const document = await pool.run(getDocument, url);
+	const language = document.querySelector(".js-review-body").getAttribute("lang");
+	const paragraphs = document.querySelectorAll(".js-review-body p");
+	const review = paragraphs.map((paragraph) => paragraph.textContent).join("\n\n");
+	const spoiler = !!document.querySelector("div.contains-spoilers");
 	return {
-		reviews,
-		next: next && `${baseUrl}${next}`,
+		tags: document.querySelectorAll("ul.tags li a").map((element) => element.textContent),
+		language,
+		text: review,
+		spoiler,
 	};
 };
 
-void (async function () {
-	console.log(JSON.stringify(await scrapePage(url), null, "\t"));
-})();
+const getImages = async (url: string, pool: AsyncPool): Promise<Images> => {
+	const document = await pool.run(getDocument, url);
+	const backdropElement = document.querySelector("#backdrop");
+	const backdrop =
+		backdropElement?.getAttribute("data-backdrop2x") ??
+		backdropElement?.getAttribute("data-backdrop") ??
+		backdropElement?.getAttribute("data-backdrop-mobile") ??
+		null;
+	const poster = document.querySelector("div.film-poster img")?.getAttribute("src") ?? null;
+	return { backdrop, poster };
+};
+
+const completeReview = async (entry: LetterboxdInfo, pool: AsyncPool): Promise<CompletedInfo> => {
+	const [completion, images] = await Promise.all([getReviewInfo(entry.url, pool), getImages(entry.movie.url, pool)]);
+	return {
+		...entry,
+		...completion,
+		movie: {
+			...entry.movie,
+			...images,
+		},
+	};
+};
+
+const scrapeReviewListPages = async (url: string, pool: AsyncPool): Promise<CompletedInfo[]> => {
+	const { reviews, next } = await scrapeReviewListPage(url, pool);
+	const futureCompleteReviews = Promise.all(reviews.map((entry) => completeReview(entry, pool)));
+	const futureRemainder: Promise<CompletedInfo[]> = next ? scrapeReviewListPages(next, pool) : Promise.resolve([]);
+	const [complete, remainder] = await Promise.all([futureCompleteReviews, futureRemainder]);
+	return [...complete, ...remainder];
+};
+
+const fetchReviews = async () => {
+	const pool = new AsyncPool(10);
+	const interval = setInterval(() => console.log(`Executing: ${pool.executing}. Queued: ${pool.queued}`), 2000);
+	const reviews = await scrapeReviewListPages(url, pool);
+	clearInterval(interval);
+	const entries = Object.fromEntries(reviews.map((review) => [review.url, review]));
+	await fs.writeFile(path.join(__dirname, "entries.json"), JSON.stringify(entries, null, "\t"));
+	console.log("Done!");
+	process.exit(0);
+};
+
+void fetchReviews();
---
+++ scripts/util/async-pool.ts
@@ -0,0 +1,82 @@
+export default class AsyncPool {
+	public static map<T extends readonly unknown[] | [], U>(
+		values: T,
+		callback: (v: T[number]) => U,
+		concurrency: number,
+	): Promise<{ -readonly [P in keyof T]: Awaited<U> }>;
+	public static map<T, U>(values: Iterable<T>, callback: (v: T) => U, concurrency: number): Promise<Awaited<U>[]>;
+	public static map<T, U>(values: Iterable<T>, callback: (v: T) => U, concurrency: number): Promise<unknown> {
+		return Promise.all(this.mapIterable(values, callback, concurrency));
+	}
+
+	public static mapToSettled<T extends readonly unknown[] | [], U>(
+		values: T,
+		callback: (v: T[number]) => U,
+		concurrency: number,
+	): Promise<{ -readonly [P in keyof T]: PromiseSettledResult<Awaited<U>> }>;
+	public static mapToSettled<T, U>(
+		values: Iterable<T>,
+		callback: (v: T) => U,
+		concurrency: number,
+	): Promise<PromiseSettledResult<Awaited<U>>[]>;
+	public static mapToSettled<T, U>(values: Iterable<T>, callback: (v: T) => U, concurrency: number): Promise<unknown> {
+		return Promise.allSettled(this.mapIterable(values, callback, concurrency));
+	}
+
+	private static *mapIterable<T, U>(
+		values: Iterable<T>,
+		callback: (v: T) => U,
+		concurrency: number,
+	): Generator<Promise<U>> {
+		const pool = new AsyncPool(concurrency);
+		for (const value of values) {
+			yield pool.run(() => callback(value));
+		}
+	}
+
+	private readonly queue: Array<() => void> = [];
+	private running: Set<Promise<unknown>> = new Set<Promise<unknown>>();
+	constructor(private readonly concurrency: number) {
+		if (!(concurrency > 0)) {
+			// Don't flip this! This also filters out NaN and null
+			throw new TypeError("Concurrency must be a positive number");
+		}
+	}
+
+	private next(): void {
+		if (this.running.size < this.concurrency) {
+			this.queue.shift()?.();
+		}
+	}
+
+	public run<Parameters extends any[], T>(
+		process: (...args: Parameters) => T,
+		...args: Parameters
+	): Promise<Awaited<T>> {
+		return new Promise((resolve, reject) => {
+			this.queue.push((): void => {
+				const promise: Promise<unknown> = (async (): Promise<Awaited<T>> => await process(...args))()
+					.finally(() => this.running.delete(promise))
+					.finally(() => this.next())
+					.then(resolve)
+					.catch(reject);
+				this.running.add(promise);
+			});
+			return this.next();
+		});
+	}
+
+	public get executing(): number {
+		return this.running.size;
+	}
+
+	public get queued(): number {
+		return this.queue.length;
+	}
+
+	public async drain(): Promise<void> {
+		while (this.executing + this.queued) {
+			await Promise.all(this.running);
+		}
+	}
+}
