8ce931158073635ab8c740e3f23242919419efea
braxtonhall <braxtonjhall@gmail.com>
2025-04-19 18:24:25 +0200

    Add touch screen support for opening and closing history

--- src/history.ts
+++ src/history.ts
@@ -29,3 +29,40 @@ export const addToHistory = async (review: Review) => {
 		}
 	}
 };
+
+export const addHistoryUIControl = () => {
+	const { historyToggle } = getElements();
+
+	const toggleFromMovement = ({ deltaX, deltaY }: { deltaX: number; deltaY: number }) => {
+		if (Math.abs(deltaY) > Math.abs(deltaX)) {
+			if (deltaY < 0) {
+				historyToggle.checked = true;
+			} else if (deltaY > 0) {
+				historyToggle.checked = false;
+			}
+		}
+	};
+
+	window.addEventListener("wheel", (event) => toggleFromMovement(event));
+
+	let lastTouch: { x: number; y: number } | null = null;
+	window.addEventListener("touchend", () => (lastTouch = null));
+	window.addEventListener("touchstart", (event) => {
+		lastTouch = {
+			x: event.touches[0].clientX,
+			y: event.touches[0].clientY,
+		};
+	});
+	window.addEventListener("touchmove", (event) => {
+		if (lastTouch) {
+			toggleFromMovement({
+				deltaX: lastTouch.x - event.touches[0].clientX,
+				deltaY: lastTouch.y - event.touches[0].clientY,
+			});
+		}
+		lastTouch = {
+			x: event.touches[0].clientX,
+			y: event.touches[0].clientY,
+		};
+	});
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import "./index.sass";
 import { setMovie } from "./setMovie";
 import { showUI } from "./showUI";
-import { getElements } from "./elements";
 import reviews from "./reviews.json";
-import { addToHistory } from "./history";
+import { addHistoryUIControl, addToHistory } from "./history";
 
 window.addEventListener("pageshow", () => {
 	const entries = Object.entries(reviews);
@@ -11,15 +10,5 @@ window.addEventListener("pageshow", () => {
 	setMovie(review);
 	void addToHistory(review);
 	showUI();
-
-	const { historyToggle } = getElements();
-	window.addEventListener("wheel", (event) => {
-		if (Math.abs(event.deltaY) > Math.abs(event.deltaX)) {
-			if (event.deltaY < 0) {
-				historyToggle.checked = true;
-			} else if (event.deltaY > 0) {
-				historyToggle.checked = false;
-			}
-		}
-	});
+	addHistoryUIControl();
 });
