aa7d9aca7a4d3d4dc1f5568fdf8120a32f38febb
braxtonhall <braxtonjhall@gmail.com>
2025-04-19 20:26:08 +0200

    Restore random posters, add poster aspect ratio

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import reviews from "./reviews.json";
 import { addHistoryUIControl, addToHistory } from "./history";
 
 window.addEventListener("pageshow", () => {
-	const entries = Object.entries(reviews).filter(([_, review]) => review.movie.poster === null);
+	const entries = Object.entries(reviews);
 	const [_, review] = entries[Math.floor(Math.random() * entries.length)];
 	setMovie(review);
 	void addToHistory(review);
