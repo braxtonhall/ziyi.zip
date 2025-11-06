c6025934ac5b1a58c9c4370d1b53974a5184d481
braxtonhall <braxtonjhall@gmail.com>
2025-11-06 13:41:56 +0100

    Cache images

--- src/setImage.ts
+++ src/setImage.ts
@@ -1,5 +1,32 @@
+import storage from "./storage";
+
 const BLACK =
 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=";
+
+const cacheImage = async (url: string): Promise<string> => {
+	const cached = (await storage.get(url)) as string | undefined;
+	if (cached) {
+		return `data:image/png;base64,${cached}`;
+	} else {
+		const response = await fetch(`https://corsproxy.io/?${url}`);
+		const blob = await response.blob();
+		const result = await new Promise<string>((resolve, reject) => {
+			const reader = new FileReader();
+			reader.addEventListener("loadend", (event) => {
+				if (typeof event.target?.result === "string") {
+					const string = event.target.result.split(",")[1];
+					resolve(string);
+				} else {
+					reject(new Error("Cannot get data"));
+				}
+			});
+			reader.readAsDataURL(blob);
+		});
+		// TODO no! the cache is growing infinitely -- BAD!
+		await storage.set(url, result);
+		return `data:image/png;base64,${result}`;
+	}
+};
 
 export const setImage = (options: { element: HTMLImageElement; src: string | null; alt?: string }) => {
 	if (options.alt) {
@@ -9,5 +36,12 @@ export const setImage = (options: { element: HTMLImageElement; src: string | nul
 		once: true,
 		passive: true,
 	});
-	options.element.src = options.src ?? BLACK;
+	if (options.src) {
+		const original = options.src;
+		cacheImage(original)
+			.catch(() => original)
+			.then((src) => (options.element.src = src));
+	} else {
+		options.element.src = BLACK;
+	}
 };
