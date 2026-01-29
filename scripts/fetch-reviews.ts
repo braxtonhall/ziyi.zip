791082a04dbc8e3777af075ab4e7c5e782686f3c
braxtonhall <braxtonjhall@gmail.com>
2026-01-29 12:16:37 +0100

    Revert "Increase timeout to 60s"
    This reverts commit dda43f463ae6111ebfb9f3d6d7b1424f3c36c247.

--- scripts/fetch-reviews.ts
+++ scripts/fetch-reviews.ts
@@ -46,7 +46,6 @@ type Images = { poster: string | null; backdrop: string | null };
 const futureBrowser = puppeteer.launch({
 	executablePath: executablePath(),
 	args: ["--no-sandbox", "--disable-setuid-sandbox"],
-	timeout: 60_000,
 });
 
 const pool = new AsyncPool(10);
